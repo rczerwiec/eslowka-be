f9be7074b0d39e9db09c7341e26533a304b23cfb
rczerwiec <czerwiecr1999@gmail.com>
2025-09-15 22:27:28 +0200

    fix main.ts again

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // Włączenie globalnego CORS z poprawnym origin i credentials
-  app.enableCors({
-    origin: "http://192.168.18.3:3000", // adres frontendu
-    credentials: true,                   // pozwala na ciasteczka/autoryzację
-  });
-
-  // Włączenie globalnego walidatora DTO
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-
-  // Opcjonalnie: jeśli chcesz ustawić globalny prefix dla wszystkich endpointów
-  // app.setGlobalPrefix('api');
-
   await app.listen(3000);
-  console.log(`Backend działa na http://localhost:3000`);
 }
-
 bootstrap();
