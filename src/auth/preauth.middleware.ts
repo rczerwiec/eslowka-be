1990af1798e99549acddd6e7c44a9b474169bbee
rczerwiec <czerwiecr1999@gmail.com>
2025-09-15 21:45:13 +0200

    fix dockerfile

--- src/auth/preauth.middleware.ts
+++ src/auth/preauth.middleware.ts
@@ -9,33 +9,33 @@ let firebase_params;
 
 // Sprawdź czy jesteśmy w Dockerze (NODE_ENV=production w docker-compose.yml)
 // if (process.env.NODE_ENV === 'production') {
-//   // DOCKER - użyj zmiennych środowiskowych
-//   firebase_params = {
-//     type: "service_account",
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-//     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-//     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//     clientId: process.env.FIREBASE_CLIENT_ID,
-//     authUri: "https://accounts.google.com/o/oauth2/auth",
-//     tokenUri: "https://oauth2.googleapis.com/token",
-//     authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
-//     clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-//   };
-// } else {
-  // LOKALNIE - użyj importowanego pliku
+  // DOCKER - użyj zmiennych środowiskowych
   firebase_params = {
-    type: serviceAccount.type,
-    projectId: serviceAccount.project_id,
-    privateKeyId: serviceAccount.private_key_id,
-    privateKey: serviceAccount.private_key,
-    clientEmail: serviceAccount.client_email,
-    clientId: serviceAccount.client_id,
-    authUri: serviceAccount.auth_uri,
-    tokenUri: serviceAccount.token_uri,
-    authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-    clientC509CertUrl: serviceAccount.client_x509_cert_url,
+    type: "service_account",
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: "https://accounts.google.com/o/oauth2/auth",
+    tokenUri: "https://oauth2.googleapis.com/token",
+    authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
   };
+// } else {
+//   // LOKALNIE - użyj importowanego pliku
+//   firebase_params = {
+//     type: serviceAccount.type,
+//     projectId: serviceAccount.project_id,
+//     privateKeyId: serviceAccount.private_key_id,
+//     privateKey: serviceAccount.private_key,
+//     clientEmail: serviceAccount.client_email,
+//     clientId: serviceAccount.client_id,
+//     authUri: serviceAccount.auth_uri,
+//     tokenUri: serviceAccount.token_uri,
+//     authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+//     clientC509CertUrl: serviceAccount.client_x509_cert_url,
+//   };
 // }
 
 @Injectable()
