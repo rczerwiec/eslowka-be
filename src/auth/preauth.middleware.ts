e3e6e529b9e10735a19967c1f39f58ef608a2ce3
rczerwiec <czerwiecr1999@gmail.com>
2025-09-15 21:54:07 +0200

    fix dockerfile

--- src/auth/preauth.middleware.ts
+++ src/auth/preauth.middleware.ts
@@ -2,47 +2,24 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 import * as firebase from 'firebase-admin';
 
-// ⬇️ import JSON-a jako moduł (działa tylko po dodaniu resolveJsonModule)
-import serviceAccount from './firebaseServiceAccount.json';
-
 let firebase_params;
 
-// Sprawdź czy jesteśmy w Dockerze (NODE_ENV=production w docker-compose.yml)
-// if (process.env.NODE_ENV === 'production') {
-  // DOCKER - użyj zmiennych środowiskowych
-  firebase_params = {
-    type: "service_account",
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    clientId: process.env.FIREBASE_CLIENT_ID,
-    authUri: "https://accounts.google.com/o/oauth2/auth",
-    tokenUri: "https://oauth2.googleapis.com/token",
-    authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
-    clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  };
-// } else {
-//   // LOKALNIE - użyj importowanego pliku
-//   firebase_params = {
-//     type: serviceAccount.type,
-//     projectId: serviceAccount.project_id,
-//     privateKeyId: serviceAccount.private_key_id,
-//     privateKey: serviceAccount.private_key,
-//     clientEmail: serviceAccount.client_email,
-//     clientId: serviceAccount.client_id,
-//     authUri: serviceAccount.auth_uri,
-//     tokenUri: serviceAccount.token_uri,
-//     authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-//     clientC509CertUrl: serviceAccount.client_x509_cert_url,
-//   };
-// }
+// Sprawdź, czy zmienna środowiskowa istnieje
+if (!process.env.FIREBASE_CRED_B64) {
+  throw new Error('Missing FIREBASE_CRED_B64 environment variable!');
+}
+
+// Dekodowanie JSON z base64
+firebase_params = JSON.parse(
+  Buffer.from(process.env.FIREBASE_CRED_B64, 'base64').toString('utf8')
+);
 
 @Injectable()
 export class PreauthMiddleware implements NestMiddleware {
-  private defaultApp: any;
+  private defaultApp: firebase.app.App;
 
   constructor() {
+    // Inicjalizacja Firebase
     this.defaultApp = firebase.initializeApp({
       credential: firebase.credential.cert(firebase_params),
       databaseURL: 'https://fir-auth-bd895.firebaseio.com',
@@ -50,23 +27,26 @@ export class PreauthMiddleware implements NestMiddleware {
   }
 
   use(req: Request, res: Response, next: Function) {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.startsWith('Bearer ')
+      ? authHeader.split(' ')[1]
+      : null;
 
-    if (token && token !== '') {
-      this.defaultApp
-        .auth()
-        .verifyIdToken(token)
-        .then(async (decodedToken) => {
-          req['user'] = { email: decodedToken.email };
-          next();
-        })
-        .catch((error) => {
-          console.error(error);
-          this.accessDenied(req.url, res);
-        });
-    } else {
-      this.accessDenied(req.url, res);
+    if (!token) {
+      return this.accessDenied(req.url, res);
     }
+
+    this.defaultApp
+      .auth()
+      .verifyIdToken(token)
+      .then(decodedToken => {
+        req['user'] = { email: decodedToken.email };
+        next();
+      })
+      .catch(error => {
+        console.error(error);
+        this.accessDenied(req.url, res);
+      });
   }
 
   private accessDenied(url: string, res: Response) {
